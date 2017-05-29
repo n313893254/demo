9c3e793f147d4bbcfdd9eb9e70723523df0a22d3
n313893254 <n313893254@126.com>
2017-05-29 10:45:30 +0800

    登录和注册

---
+++ vue-todoV1/app/app.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todoList: [],
+    actionType: 'signUp'
+  },
+
+  created: function() {
+    let oldDataString = window.localStorage.getItem('myTodos');
+    let oldData = JSON.parse(oldDataString);
+    this.todoList = oldData || [];
+
+    let oldUserInput = window.localStorage.getItem('userInput');
+    this.newTodo = oldUserInput || [];
+
+    window.onbeforeunload = ()=>{
+      //保存todo
+      let dataString = JSON.stringify(this.todoList);
+      window.localStorage.setItem('myTodos', dataString);
+
+      //保存input
+      let userInput = this.newTodo;
+      window.localStorage.setItem('userInput', userInput);
+    }
+  },
+
+  methods: {
+    //增加待办
+    addTodo: function() {
+      this.todoList.push({
+        title: this.newTodo,
+        createdAt: '待办事项创建于' + new Date(),
+        done: false
+      })
+      //加入数组后置空
+      this.newTodo = '';
+    },
+
+    //删除待办
+    removeTodo: function(todo) {
+      let index = this.todoList.indexOf(todo);
+      this.todoList.splice(index, 1);
+    }
+  }
+})
