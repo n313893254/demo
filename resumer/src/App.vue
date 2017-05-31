8e103f9fa85da48feb48e7085928a57832e120af
n313893254 <n313893254@126.com>
2017-05-31 14:41:17 +0800

    first push

---
+++ resumer/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      text: '你好'
+    }
+  }
+}
+</script>
+
+<style>
+p {
+  color: red;
+}
+</style>
