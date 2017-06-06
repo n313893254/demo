0bfd78f48f0db42a2d772a56030b0fd7fff31903
n313893254 <n313893254@126.com>
2017-06-06 11:13:14 +0800

    使用 leancloud 注册

--- resumer/src/components/Topbar.vue
+++ resumer/src/components/Topbar.vue
@@ -3,9 +3,10 @@
     <div class="wrapper">
       <span class="logo">Resumer</span>
       <div class="actions">
+        <span>{{ user }}</span>
         <a href="#" class="button primary" @click.prevent="signUpDialogVisible = true">注册</a>
         <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
-          我就是slot内容
+          <SignUpForm @success="login($event)"/>
         </MyDialog>
         <a href="#" class="button">登录</a>
         <button  class="primary">保存</button>
@@ -17,6 +18,8 @@
 
 <script>
 import MyDialog from './MyDialog'
+import SignUpForm from './SignUpForm'
+
 export default {
   name: 'Topbar',
   data () {
@@ -24,8 +27,19 @@ export default {
       signUpDialogVisible: false
     }
   },
+  computed: {
+    user () {
+      return this.$store.state.user
+    }
+  },
   components: {
-    MyDialog
+    MyDialog, SignUpForm
+  },
+  methods: {
+    login (user) {
+      this.signUpDialogVisible = false
+      this.$store.commit('setUser', user)
+    }
   }
 }
 </script>
---
+++ resumer/src/lib/leancloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'I5L75HKgJ1rIkVFupOACInbM-gzGzoHsz'
+var APP_KEY = 'VvW6tk0GK3PRRT0rmOWVjrn7'
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+export default AV
