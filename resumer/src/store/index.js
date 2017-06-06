5d60d73773971de4994039202d56f70128f032af
n313893254 <n313893254@126.com>
2017-06-06 21:30:38 +0800

    删除所有假数据

--- resumer/src/store/index.js
+++ resumer/src/store/index.js
@@ -21,30 +21,26 @@ export default new Vuex.Store({
         { field: 'contacts', icon: 'phone' }
       ],
       profile: {
-        name: 'poi酱',
-        city: '大城市铁岭',
-        title: '首席装逼师',
-        birthday: '1990-01-01'
+        name: '',
+        city: '',
+        title: '',
+        birthday: ''
       },
       workHistory: [
-        {company: '鸡飞狗跳公司', content: '公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。我的主要工作如下:1. 完成既定产品需求。2. 修复 bug。'},
-        {company: '狗急跳墙责任有限公司', content: '公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。我的主要工作如下:1. 完成既定产品需求。2. 修复 bug'}
+        {company: '', content: ''},
+        {company: '', content: ''}
       ],
       education: [
-        { school: '黄志诚警官大学', content: '本科' },
-        { school: '韩琛古惑仔高中' }
+        { school: '', content: '' }
       ],
       projects: [
-        { name: 'project A', content: '文字' },
-        { name: 'project B', content: '文字' }
+        { name: '', content: '' }
       ],
       awards: [
-        { name: 'awards A', content: '文字' },
-        { name: 'awards B', content: '文字' }
+        { name: '', content: '' }
       ],
       contacts: [
-        { contact: 'phone', content: '13812345678' },
-        { contact: 'qq', content: '12345678' }
+        { contact: '', content: '' }
       ]
     }
   },
