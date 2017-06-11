1ce1804ac092e9f0cc48a6ec0615d574e178a208
n313893254 <n313893254@126.com>
2017-06-11 23:56:40 +0800

    更新删除按钮的样式

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -98,5 +98,12 @@ svg.icon {
     background: #02af5f;
     color: white;
   }
+  &.small {
+    height: 24px;
+    font-size: inherit;
+    width: auto;
+    padding-left: 1em;
+    padding-right: 1em;
+  }
 }
 </style>
