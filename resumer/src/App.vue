3f9756241684d60146f45bf5697eacb9b70418e3
n313893254 <n313893254@126.com>
2017-06-11 23:48:37 +0800

    将 button 的样式提取到 App，公用

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -42,40 +42,61 @@ export default {
 </script>
 
 <style lang="scss">
-  .page {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    background: #EAEBEC;
-    >main {
-      flex-grow: 1;
-    }
-    >main {
-    min-width: 1024px;
-    max-width: 1440px;
-    margin-top: 16px;
-    margin-bottom: 16px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 16px;
-    width: 100%;
-    align-self: center;
-    }
-  }
-  #resumeEditor {
-    min-width: 35%;
-    background: #444;
-  }
-  #resumePreview {
+.page {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: #EAEBEC;
+  >main {
     flex-grow: 1;
-    margin-left: 16px;
-    background: #777;
   }
-  svg.icon {
-    height: 1em;
-    width: 1em;
-    fill: currentColor;
-    vertical-align: -0.1em;
-    font-size: 16px;
+  >main {
+  min-width: 1024px;
+  max-width: 1440px;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 16px;
+  width: 100%;
+  align-self: center;
   }
+}
+#resumeEditor {
+  min-width: 35%;
+  background: #444;
+}
+#resumePreview {
+  flex-grow: 1;
+  margin-left: 16px;
+  background: #777;
+}
+svg.icon {
+  height: 1em;
+  width: 1em;
+  fill: currentColor;
+  vertical-align: -0.1em;
+  font-size: 16px;
+}
+.button {
+  width: 72px;
+  height: 32px;
+  border: none;
+  cursor: pointer;
+  font-size: 18px;
+  background: #ddd;
+  color: #222;
+  text-decoration: none;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
+    box-shadow: 1px 1px 1px hsla(0, 0, 0, 0.5);
+  }
+  &.primary {
+    background: #02af5f;
+    color: white;
+  }
+}
 </style>
--- resumer/src/components/Topbar.vue
+++ resumer/src/components/Topbar.vue
@@ -84,27 +84,6 @@ export default {
   }
 }
 
-.button {
-  width: 72px;
-  height: 32px;
-  border: none;
-  cursor: pointer;
-  font-size: 18px;
-  background: #ddd;
-  color: #222;
-  text-decoration: none;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  vertical-align: middle;
-  &:hover {
-    box-shadow: 1px 1px 1px hsla(0, 0, 0, 0.5);
-  }
-  &.primary {
-    background: #02af5f;
-    color: white;
-  }
-}
 .actions {
   display: flex;
   .userActions {
