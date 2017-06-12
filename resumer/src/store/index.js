b68a4ed595a695e3b7a00bd13452738b7535856b
n313893254 <n313893254@126.com>
2017-06-12 22:44:02 +0900

    添加保存按钮

--- resumer/src/store/index.js
+++ resumer/src/store/index.js
@@ -1,6 +1,8 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
 import objectPath from 'object-path'
+import AV from '../lib/leancloud'
+import getAVUser from '../lib/getAVUser'
 
 Vue.use(Vuex)
 
@@ -19,7 +21,9 @@ export default new Vuex.Store({
       { field: 'awards', icon: 'cup', type: 'array', keys: ['name', 'details'] },
       { field: 'contacts', icon: 'phone', type: 'array', keys: ['contact', 'details'] }
     ],
-    resume: {}
+    resume: {
+      id: ''
+    }
   },
   mutations: {
     initState (state, payload) {
@@ -58,6 +62,35 @@ export default new Vuex.Store({
     },
     removeResumeSubfield (state, {field, index}) {
       state.resume[field].splice(index, 1)
+    },
+    setResumeId (state, { id }) {
+      state.resume.id = id
+    }
+  },
+  actions: {
+    saveResume ({ state, commit }, payload) {
+      var Resume = AV.Object.extend('Resume')
+      if (state.resume.id) {} else {
+        var resume = new Resume()
+        resume.set('profile', state.resume.profile)
+        resume.set('workHistory', state.resume.workHistory)
+        resume.set('education', state.resume.education)
+        resume.set('projects', state.resume.projects)
+        resume.set('awards', state.resume.awards)
+        resume.set('contacts', state.resume.contacts)
+        resume.set('owner_id', getAVUser().id)
+
+        var acl = new AV.ACL()
+        acl.setPublicReadAccess(true)
+        acl.setWriteAccess(AV.User.current(), true)
+
+        resume.setACL(acl)
+        resume.save().then(function (response) {
+          commit('setResumeId', { id: response.id })
+        }).catch(function (error) {
+          console.log(error)
+        })
+      }
     }
   }
 })
