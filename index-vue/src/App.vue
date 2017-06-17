1f52c6d5c30582cfeea78965111fd96f4676c3bc
n313839254 <n313893254@git.com>
2017-06-17 22:13:11 +0800

    Topbar: html

--- index-vue/src/App.vue
+++ index-vue/src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <Topbar/>
   </div>
 </template>
 
 <script>
+import Topbar from './components/Topbar'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: { Topbar }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+#app {}
 </style>
---
+++ index-vue/src/components/Topbar.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div id="topbar">
+    <div class="wrapper">
+      <span class="logo">Kancollect</span>
+      <div class="action">
+        <span>日常</span>
+        <span>舰娘</span>
+        <span>时间轴</span>
+        <span>主力舰队</span>
+        <span>提督热线</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Topbar',
+  data () {
+    return {
+      message: 'Topbar'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
