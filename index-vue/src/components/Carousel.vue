2d7812f65103aab338c64e15312ede5837acab71
n313839254 <n313893254@git.com>
2017-06-18 13:50:04 +0800

    finish: carousel

--- index-vue/src/components/Carousel.vue
+++ index-vue/src/components/Carousel.vue
@@ -1,6 +1,10 @@
 <template lang="html">
-  <div class="">
-    {{ message }}
+  <div id="carousel">
+    <el-carousel trigger="click" height="700px">
+      <el-carousel-item v-for="item in images" :key="item">
+        <img :src="item.url" alt="">
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
@@ -9,11 +13,27 @@ export default {
   name: 'Carousel',
   data () {
     return {
-      message: 'Carousel'
+      message: 'Carousel',
+      images: [
+        {
+          url: 'http://omph2coqc.bkt.clouddn.com/61990654_p0%20%281%29.jpg'
+        },
+        {
+          url: 'http://omph2coqc.bkt.clouddn.com/175548qjhhicxqzhfxijhf.jpg'
+        },
+        {
+          url: 'http://omph2coqc.bkt.clouddn.com/thumb-1920-736033.jpg'
+        }
+      ]
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+#carousel {
+  img {
+    width: 100%;
+  }
+}
 </style>
--- index-vue/src/main.js
+++ index-vue/src/main.js
@@ -1,7 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import { Button, Select, Dialog, Menu, MenuItem, Row, Col } from 'element-ui'
+import {
+  Button,
+  Select,
+  Dialog,
+  Menu,
+  MenuItem,
+  Row,
+  Col,
+  Carousel,
+  CarouselItem
+} from 'element-ui'
 import App from './App'
 
 Vue.config.productionTip = false
@@ -13,6 +23,8 @@ Vue.use(Menu)
 Vue.use(MenuItem)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
 
 /* eslint-disable no-new */
 new Vue({
