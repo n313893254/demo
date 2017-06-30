9d7762220403cb87c2370343ae53556b3051becb
n313839254 <n313893254@git.com>
2017-06-30 21:34:22 +0800

    add leancloud.js

---
+++ todo-react/src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+const APP_ID = 'uCUyyf8BRyQaxO6xSqIqIX4v-gzGzoHsz'
+const APP_KEY = 'bolW8RSewIqofSenm5DuCOaD'
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+export default AV
