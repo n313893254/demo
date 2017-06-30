4a19350b2871160ea290e2b2027d3d768fd104bb
n313839254 <n313893254@git.com>
2017-06-30 21:35:37 +0800

    delete localstorage

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -4,14 +4,13 @@ import './reset.css'
 import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
-import * as localStore from './localStore'
 
 class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
       newTodo: '',
-      todoList: localStore.load('todoList') || []
+      todoList: []
     }
   }
   render() {
@@ -72,7 +71,6 @@ class App extends Component {
   }
 
   componentDidUpdate () {
-    localStore.save('todoList', this.state.todoList)
   }
 }
 
