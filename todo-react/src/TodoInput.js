01068a19a185b5d6a89ccde787aab77e6bcfc552
n313839254 <n313893254@git.com>
2017-06-30 15:55:54 +0800

    add onSubmit

--- todo-react/src/TodoInput.js
+++ todo-react/src/TodoInput.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react'
 export default class TodoInput extends Component {
   render () {
     return <input type="text" defaultValue={this.props.content}
-      onKeyPress={this.submit}/>
+      onKeyPress={this.submit.bind(this)}/>
   }
 
   submit (e) {
     if (e.key === 'Enter') {
-      console.log('用户按回车')
+      this.props.onSubmit.call()
     }
   }
 }
