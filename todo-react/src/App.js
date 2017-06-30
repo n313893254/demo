47389ad16d385a4b62c3dde76e2c628eca8a0183
n313839254 <n313893254@git.com>
2017-06-30 16:27:11 +0800

    新增todo

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -9,23 +9,14 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      newTodo: 'test',
-      todoList: [
-        {
-          id: 1,
-          title: '第一个代办'
-        },
-        {
-          id: 2,
-          title: '第二个代办'
-        }
-      ]
+      newTodo: '',
+      todoList: []
     }
   }
   render() {
     let todos = this.state.todoList.map((item, index) => {
       return (
-        <li>
+        <li key={index}>
           <TodoItem todo={item}/>
         </li>
       )
@@ -35,7 +26,7 @@ class App extends Component {
       <div className="App">
         <h1>我的代办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} />
+          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol>
           {todos}
@@ -44,9 +35,25 @@ class App extends Component {
     );
   }
 
-  addTodo () {
-    console.log('添加todo')
+  addTodo (event) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
   }
+}
+
+let id = 0
+
+function idMaker () {
+  id += 1
+  return id
 }
 
 export default App;
