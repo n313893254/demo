877b6bc73a8e1c8b0c7fb1c0107f19df2fca7602
n313839254 <n313893254@git.com>
2017-07-01 16:55:40 +0800

    login

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -41,7 +41,11 @@ class App extends Component {
         <ol className="todoWrapper">
           {todos}
         </ol>
-        {this.state.user.id ? null : <UserDialog onSignUp={this.onSignUp.bind(this)}/>}
+        {this.state.user.id ?
+           null :
+           <UserDialog
+            onSignUp={this.onSignUp.bind(this)}
+            onSignIn={this.onSignIn.bind(this)}/>}
       </div>
     );
   }
@@ -88,6 +92,11 @@ class App extends Component {
     signOut()
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.user = {}
+    this.setState(stateCopy)
+  }
+  onSignIn (user) {
+    let stateCopy = JSON.parse(JSON.stringify(this.state))
+    stateCopy.user = user
     this.setState(stateCopy)
   }
 }
