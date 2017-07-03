1379b70522f2f69f0dbd7aff27ade7f372814a56
n313839254 <n313893254@git.com>
2017-07-03 16:17:05 +0800

    初步轮廓

---
+++ 3d/js/index.js
@@ -0,0 +1,46 @@
+$(function () {
+  var imgL = $(".pic img").length;
+  var deg = 360 / imgL;
+  var roY = 0, roX = -10;
+  var xN = 0, yN = 0;
+  var play = null;
+
+  $(".pic img").each(function (i) {
+    $(this).css({
+      'transform': "rotateY("+i*deg+"deg) translateZ(300px);"
+    })
+    $(this).attr('ondragstart', 'return false')
+  })
+
+  $(document).mousedown(function (ev) {
+    var x_ = ev.clientX;
+    var y_ = ev.clientY;
+    clearInterval(play);
+    $(this).bind('mousemove', function (ev) {
+      var x = ev.clientX;
+      var y = ev.clientY;
+
+      xN = x - x_;
+      yN = y - y_;
+
+      roY += xN * 0.2;
+      roX -= yN * 0.1;
+
+      $('.pic').css({
+        transform: 'perspective(800px) rotateX('+roX+'deg) rotateY('+roY+'deg)'
+      })
+
+      x_ = ev.clientX;
+      y_ = ev.clientY;
+    })
+  }).mouseup(function () {
+    $(this).unbind('mousemove')
+    var play = setInterval(function () {
+      xN *= 0.95;
+      yN *= 0.1;
+      $('.pic').css({
+        transform: 'perspective(800px) rotateX('+roX+'deg) rotateY('+roY+'deg)'
+      })
+    }, 30)
+  })
+})
