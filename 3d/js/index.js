54deace5e06ac54a4500cb55c3c06c8c195d1f0e
n313839254 <n313893254@git.com>
2017-07-03 21:14:34 +0800

    scroll

--- 3d/js/index.js
+++ 3d/js/index.js
@@ -6,9 +6,7 @@ $(function () {
   var play = null;
 
   lightbox.option({
-    'fitImagesInViewport': false,
-    'wrapAround': true,
-    'disableScrolling': true
+    'fitImagesInViewport': false
   })
 
   $(".pic img").each(function (i) {
