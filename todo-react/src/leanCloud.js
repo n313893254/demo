0182b4207de2bea828973305979611bc1f02b033
n313839254 <n313893254@git.com>
2017-07-06 22:30:06 +0800

    add: get all todos

--- todo-react/src/leanCloud.js
+++ todo-react/src/leanCloud.js
@@ -10,6 +10,17 @@ AV.init({
 export default AV
 
 export const TodoModel = {
+  getByUser (user, successFn, errorFn) {
+    let query = new AV.Query('Todo')
+    query.find().then((response) => {
+      let array = response.map((t) => {
+        return {id: t.id, ...t.attributes}
+      })
+      successFn.call(null, array)
+    }, (error) => {
+      errorFn && errorFn.call(null, error)
+    })
+  },
   create({status, title, deleted}, successFn, errorFn) {
     let Todo = AV.Object.extend('Todo')
     let todo = new Todo()
@@ -22,11 +33,9 @@ export const TodoModel = {
       errorFn && errorFn.call(null, error)
     })
   },
-
   update () {
 
   },
-
   destroy () {
 
   }
