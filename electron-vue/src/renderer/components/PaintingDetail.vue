742177d488a49f06b57d227cda48dfa5e38176d4
n313839254 <n313893254@git.com>
2017-07-14 17:07:17 +0800

    add: painting to article

--- electron-vue/src/renderer/components/PaintingDetail.vue
+++ electron-vue/src/renderer/components/PaintingDetail.vue
@@ -2,6 +2,7 @@
   <div class="">
     <div class="box">
       <p>这是画作详情页</p>
+      <router-link class="button" to="/Painting/ArticleDetail">这是一篇文章</router-link>
       <button type="button" name="button" @click="pageBack()">X</button>
     </div>
   </div>
