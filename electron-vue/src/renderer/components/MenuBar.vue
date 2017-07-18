13e6b1e83c4544d1de11498b1149ac52c152a6d1
n313839254 <n313893254@git.com>
2017-07-18 10:18:54 +0800

    fix: left & right

--- electron-vue/src/renderer/components/MenuBar.vue
+++ electron-vue/src/renderer/components/MenuBar.vue
@@ -1,15 +1,15 @@
 <template lang="html">
   <div class="menu-wrapper-right">
-    <div class="menu" v-if="leftMenuSeen">
+    <div class="menu" v-if="rightMenuSeen">
       <router-link class="button" to="/Chronological">年表</router-link>
       <router-link class="button" to="/painting">画作</router-link>
       <router-link class="button" to="/history">史料</router-link>
       <router-link class="button" to="/Research">研究</router-link>
       <router-link class="button" to="/Search">搜索</router-link>
-      <div class="button" @click="closeLeftMenu()">返回</div>
+      <div class="button" @click="closeRightMenu()">返回</div>
     </div>
     <div class="menu" v-else>
-      <div class="button" @click="openLeftMenu()">导航菜单</div>
+      <div class="button" @click="openRightMenu()">导航菜单</div>
     </div>
   </div>
 </template>
@@ -18,16 +18,16 @@
 export default {
   name: 'MenuBar',
   computed: {
-    leftMenuSeen () {
-      return this.$store.state.leftMenuSeen
+    rightMenuSeen () {
+      return this.$store.state.rightMenuSeen
     }
   },
   methods: {
-    openLeftMenu () {
-      return this.$store.commit('openLeftMenu')
+    openRightMenu () {
+      return this.$store.commit('openRightMenu')
     },
-    closeLeftMenu () {
-      return this.$store.commit('closeLeftMenu')
+    closeRightMenu () {
+      return this.$store.commit('closeRightMenu')
     }
   }
 }
--- electron-vue/src/renderer/components/Painting.vue
+++ electron-vue/src/renderer/components/Painting.vue
@@ -2,7 +2,7 @@
   <div class="">
     <MenuBar/>
     <div class="menu-wrapper-left">
-      <div class="menu" v-if="rightMenuSeen">
+      <div class="menu" v-if="leftMenuSeen">
         <div class="button" @click="set('书法')">书法</div>
         <div class="button" @click="set('连环画')">连环画</div>
         <div class="button" @click="set('速写')">速写</div>
@@ -10,10 +10,10 @@
         <div class="button" @click="set('临摹')">临摹</div>
         <div class="button" @click="set('山水')">山水</div>
         <div class="button" @click="set('花鸟')">花鸟</div>
-        <div class="button" @click="closeRightMenu()">返回</div>
+        <div class="button" @click="closeLeftMenu()">返回</div>
       </div>
       <div class="menu" v-else>
-        <div class="button" @click="openRightMenu()">分类菜单</div>
+        <div class="button" @click="openLeftMenu()">分类菜单</div>
       </div>
     </div>
     <div class="box">
@@ -33,19 +33,19 @@ export default {
     title () {
       return this.$store.state.paintingTitle
     },
-    rightMenuSeen () {
-      return this.$store.state.rightMenuSeen
+    leftMenuSeen () {
+      return this.$store.state.leftMenuSeen
     }
   },
   methods: {
     set (value) {
       return this.$store.commit('setPaintingList', value)
     },
-    openRightMenu () {
-      return this.$store.commit('openRightMenu')
+    openLeftMenu () {
+      return this.$store.commit('openLeftMenu')
     },
-    closeRightMenu () {
-      return this.$store.commit('closeRightMenu')
+    closeLeftMenu () {
+      return this.$store.commit('closeLeftMenu')
     }
   }
 }
