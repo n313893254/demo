4047e91e18a5388f4ce8d87a612a69093fb46387
n313839254 <n313893254@git.com>
2017-07-19 14:27:14 +0800

    update

--- electron-vue/src/renderer/components/Search.vue
+++ electron-vue/src/renderer/components/Search.vue
@@ -1,11 +1,38 @@
 <template lang="html">
-  <div class="">
+  <div class="page">
     <MenuBar/>
-    <p>这是搜索页面</p>
-    <button @click="set('画作')">画作</button>
-    <button @click="set('史料')">史料</button>
-    <button @click="set('研究')">研究</button>
-    <div class="box">
+    <div class="searchBar">
+      <label for="">年份</label>
+      <select class="" name="">
+        <option value="1993">1990</option>
+        <option value="1993">1980</option>
+        <option value="1993">1970</option>
+        <option value="1993">1960</option>
+        <option value="1993">1950</option>
+        <option value="1993">1940</option>
+      </select>
+      <label for="">月份</label>
+      <select class="" name="">
+        <option value="1">1月</option>
+        <option value="1">2月</option>
+        <option value="1">3月</option>
+        <option value="1">4月</option>
+        <option value="1">5月</option>
+        <option value="1">6月</option>
+        <option value="1">7月</option>
+        <option value="1">8月</option>
+        <option value="1">9月</option>
+        <option value="1">10月</option>
+        <option value="1">11月</option>
+        <option value="1">12月</option>
+      </select>
+    </div>
+    <div class="tab">
+      <div @click="set('画作')">画作</div>
+      <div @click="set('史料')">史料</div>
+      <div @click="set('研究')">研究</div>
+    </div>
+    <div class="panel">
         <p>这是{{ title }}页面</p>
     </div>
   </div>
@@ -31,4 +58,24 @@ export default {
 </script>
 
 <style lang="css">
+.page {
+  width: 100%;
+}
+.searchBar select {
+  margin: 5vh 5vw 5vh 1vw;
+}
+.searchBar label {
+  margin-left: 5vw;
+}
+.tab div {
+  border: 1px solid #000;
+  cursor: pointer;
+  display: inline-block;
+  margin: 0 3vw;
+}
+.panel {
+  border: 1px solid #000;
+  height: 79vh;
+  margin: 0 1.4vw;
+}
 </style>
