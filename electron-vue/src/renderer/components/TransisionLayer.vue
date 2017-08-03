e34093d24f765d4ee4cdb74d0dfb7cc7b18dda5a
n313893254 <n313893254@126.com>
2017-08-03 16:57:05 +0800

    add: transitoin

---
+++ electron-vue/src/renderer/components/TransisionLayer.vue
@@ -0,0 +1,80 @@
+<template lang="html">
+  <div class="cd-transition-layer">
+    <div class="bg-layer"></div>
+  </div>
+</template>
+
+<script>
+import $ from 'jquery'
+import turn from '../ignore_lib/turn.min'
+
+export default {
+  name: 'TransisionLayer',
+  mounted: function () {
+    console.log($('.poi'))
+    console.log(turn)
+    // 泼墨效果
+    var transitionLayer = $('.cd-transition-layer')
+    transitionLayer.addClass('closing')
+    transitionLayer.addClass('visible opening')
+    setTimeout(function () {
+      transitionLayer.addClass('no-cssanimations')
+    }, 3000)
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.cd-transition-layer {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  visibility: hidden;
+  overflow: hidden;
+}
+.cd-transition-layer .bg-layer {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translateY(-50%) translateX(-2%);
+  height: 100%;
+  width: 2500%;
+  background: url('http://omph2coqc.bkt.clouddn.com/17-7-31/72171056.jpg') no-repeat 0 0;
+  background-size: 100% 100%;
+}
+.cd-transition-layer.visible {
+  opacity: 1;
+  visibility: visible;
+}
+.cd-transition-layer.opening .bg-layer {
+  -webkit-animation: cd-sequence 2s steps(24);
+  animation-fill-mode: forwards;
+}
+.cd-transition-layer.closing .bg-layer {
+  -webkit-animation: cd-sequence-reverse 2s steps(24);
+  animation-fill-mode: forwards;
+}
+.no-cssanimations.cd-transition-layer {
+  display: none;
+}
+@-webkit-keyframes cd-sequence {
+  0% {
+    transform: translateY(-50%) translateX(-2%);
+  }
+  100% {
+    transform: translateY(-50%) translateX(-98%);
+  }
+}
+@-webkit-keyframes cd-sequence-reverse {
+  0% {
+    transform: translateY(-50%) translateX(-98%);
+  }
+  100% {
+    transform: translateY(-50%) translateX(-2%);
+  }
+}
+</style>
