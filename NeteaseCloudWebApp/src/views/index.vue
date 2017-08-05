8467ccf8d2431112724832f2a13a601044473214
n313893254 <n313893254@126.com>
2017-08-05 23:45:19 +0800

    add: index.vue

--- NeteaseCloudWebApp/src/views/index.vue
+++ NeteaseCloudWebApp/src/views/index.vue
@@ -1,13 +1,47 @@
 <template lang="html">
   <div class="">
-
+    <mu-appbar>
+      <div class="logo" slot="left"></div>
+      <mu-icon-button icon='search' slot="right"/>
+    </mu-appbar>
+    <mu-tabs :value="activeTab" class="view-tabs">
+      <mu-tab value="rage" title="时下流行"/>
+      <mu-tab value="songList" title="歌单"/>
+      <mu-tab value="leaderBoard" title="排行榜"/>
+      <mu-tab value="hotSinger" title="热门歌手"/>
+    </mu-tabs>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      activeTab: 'rage'
+    }
+  },
+  methods: {
+    handleTabChange (val) {
+      this.activeTab = val
+      this.$router.push({ path: '/index/' + val })
+    }
+  }
 }
 </script>
 
-<style lang="css">
+<style lang="less" scoped>
+.logo {
+  width: 150px;
+  height: 48px;
+  background: url('../../static/logo.png') no-repeat left center;
+  background-size: cover;
+}
+.view-tabs {
+  background-color: transparent;
+  color: rgba(0, 0, 0, 0.87);
+  .mu-tab-link {
+    color: rgba(102, 102, 102, 1);
+  }
+}
 </style>
