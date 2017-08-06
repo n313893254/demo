c008fa01abcbc2b3281fac4e0244bdfe46317f26
n313893254 <n313893254@126.com>
2017-08-06 19:54:12 +0800

    add playbar.vue

--- NeteaseCloudWebApp/src/App.vue
+++ NeteaseCloudWebApp/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <Player></Player>
   </div>
 </template>
 
 <script>
+import Player from './components/playerBar'
+export default {
+  components: { Player }
+}
 </script>
