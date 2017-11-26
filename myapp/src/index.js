125814b4de05b4f315e1a1787f8364484093d296
n313893254 <n313893254@126.com>
2017-11-26 13:34:06 +0800

    update

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import todoApp from './reducers'
+
+let store = createStore(todoApp)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
