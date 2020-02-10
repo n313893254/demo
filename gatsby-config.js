b31eb5850c900082e05e0a9e79806dc32601efc6
n313893254 <n313893254@126.com>
2020-02-10 13:49:08 +0800

    Initial commit from gatsby: (https://github.com/Rocketseat/gatsby-starter-rocket-docs.git)

---
+++ gatsby-config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `@rocketseat/gatsby-theme-docs`,
+    defaultTitle: `@rocketseat/gatsby-theme-docs`,
+    siteTitleShort: `gatsby-theme-docs`,
+    siteDescription: `Out of the box Gatsby Theme for creating documentation websites easily and quickly`,
+    siteUrl: `https://rocketdocs.netlify.com`,
+    siteAuthor: `@rocketseat`,
+    siteImage: `/banner.png`,
+    siteLanguage: `en`,
+    themeColor: `#7159c1`,
+    footer: `Theme by Rocketseat`,
+  },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocketseat Gatsby Themes`,
+        short_name: `RS Gatsby Themes`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-99997611-9`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://rocketdocs.netlify.com`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
